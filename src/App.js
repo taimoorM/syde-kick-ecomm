66e71d17475675b9a3265dc48e1192950912b50f
Taimoor <taimoor.m89@gmail.com>
2022-08-12 17:34:17 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Nav from "./Nav";
 import "./App.css";
+import Nav from "./Nav";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,13 @@
 const Header = () => {
-  return <Header></Header>;
+  return (
+    <header className="header">
+      <div className="wrapper">
+        <h2>Syde Kicks</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <button className="button">Shop Now</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
