937007e3e07bccfae8940806460b56751298b83f
Taimoor <taimoor.m89@gmail.com>
2022-08-15 19:09:04 -0400

    moved assets to public and updated relavant paths

--- src/Product.js
+++ src/Product.js
@@ -6,7 +6,7 @@ import AddProduct from "./AddProduct";
 
 const Product = ({ product }) => {
   const { img, title, discount, price } = product;
-  console.log(img);
+
   const newPrice = price - price * discount;
 
   const [show, setShow] = useState(false);
@@ -18,7 +18,10 @@ const Product = ({ product }) => {
   return (
     <div className="Product">
       <div className="productImg">
-        <img src={`./assets/${img[Object.keys(img)[0]]}`} alt="" />
+        <img
+          src={`${process.env.PUBLIC_URL}/assets${img[Object.keys(img)[0]]}`}
+          alt=""
+        />
       </div>
       <div className="productInner">
         <div className="productInfo">
