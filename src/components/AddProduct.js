dca3bf1d055b23f504e1d8c5efeeb9036197a320
Taimoor <taimoor.m89@gmail.com>
2022-08-18 03:24:53 -0400

    updated some styling

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -91,8 +91,7 @@ const AddProduct = ({ product, closeModal }) => {
             <legend>Colour:</legend>
             {product.colours.map((colour, idx) => {
               return (
-                <div key={idx}>
-                  <label htmlFor={colour}>{colour}</label>
+                <div className="colourInput" key={idx}>
                   <input
                     type="radio"
                     id={colour}
@@ -102,6 +101,7 @@ const AddProduct = ({ product, closeModal }) => {
                     checked={colourChoice === colour}
                     required
                   />
+                  <label htmlFor={colour}>{colour}</label>
                 </div>
               );
             })}
